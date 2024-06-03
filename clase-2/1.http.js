fb74046c25dedb92f28cb16b1095dbc38d0ed968
Samir Fernandez <samirfdezc@gmail.com>
2024-06-03 17:42:21 -0400

    ++

---
+++ clase-2/1.http.js
@@ -0,0 +1,36 @@
+const http = require("node:http");
+const fs = require("node:fs");
+const pc = require("picocolors");
+
+const desiredPort = process.env.PORT ?? 1234;
+
+const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+  if (req.url === "/") {
+    res.end("<h1>Bienvenido a mi página de inicio</h1>");
+  } else if (req.url === "/profile-photo.jpg") {
+    fs.readFile("./aboutPhoto.jpg", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("<h1>500 Internal Server Error</h1>");
+      } else {
+        res.setHeader("Content-Type", "image/jpg");
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/contacto") {
+    res.end("<h1>Contacto</h1>");
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>Error 404</h1>");
+  }
+};
+
+const server = http.createServer(processRequest);
+
+server.listen(desiredPort, () => {
+  console.log(
+    `server listening on ${pc.blue(`http://localhost:${desiredPort}`)}`
+  );
+});
