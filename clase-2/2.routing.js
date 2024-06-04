4449d994962fbf6d8b231fb56675490cfbb4b785
Samir Fernandez <samirfdezc@gmail.com>
2024-06-04 10:27:02 -0400

    ++

--- clase-2/2.routing.js
+++ clase-2/2.routing.js
@@ -1,8 +1,8 @@
 const http = require("node:http");
 
-const desiredPort = process.env.PORT ?? 1234;
-
 const dittoJSON = require("./pokemon/ditto.json");
+
+const desiredPort = process.env.PORT ?? 1234;
 
 const processRequest = (req, res) => {
   const { method, url } = req;
@@ -16,7 +16,38 @@ const processRequest = (req, res) => {
         default:
           res.statusCode = 404;
           res.setHeader("Content-Type", "text/html; charset=utf-8");
-          return res.end("<h1>404</h1>");
+          return res.end("<h1>404 Not Found</h1>");
+      }
+
+    case "POST":
+      switch (url) {
+        case "/pokemon": {
+          let body = "";
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+
+          req.on("end", () => {
+            const data = JSON.parse(body);
+            res.writeHead(201, {
+              "Content-Type": "application/json; charset=utf-8",
+            });
+            data.timestamp = Date.now();
+            res.end(JSON.stringify(data));
+          });
+          break;
+        }
+
+        default:
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/plain; charset=utf-8");
+          return res.end("404 Not Found");
+        //   res.setHeader("Content-Type", "application/json; charset=utf-8");
+        //   return res.end(JSON.stringify(dittoJSON));
+        // default:
+        //   res.statusCode = 404;
+        //   res.setHeader("Content-Type", "text/html; charset=utf-8");
+        //   return res.end("<h1>404 Not Found</h1>");
       }
   }
 };
